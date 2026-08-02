1cddcfe01110b4768c0c996cd5486811cfa79242
Yanaphat Chatphokhinkun <yanaphat.chatphokhinkun@gmail.com>
2026-08-02 18:49:09 +0700

    feat: redesign footer for official first launch

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
     const navItems = (isOrg) ? NavItemsOrg : NavItemsAtten;
     return (
         <footer className="w-full shadow-[0_-1px_6px_0_rgba(0,0,0,0.12)] flex flex-col gap-y-5 lg:px-25 pt-7.5 lg:pb-5 pb-2.5">
-            <div className="w-full flex lg:flex-row flex-col justify-center lg:justify-between lg:relative lg:items-center mb-5 gap-y-12.5 lg:gap-auto">
+            <div className="w-full flex lg:flex-row flex-col justify-center lg:justify-between lg:relative lg:items-center mb-5 gap-y-5 lg:gap-auto">
                 <div className="flex flex-col items-center lg:items-start gap-y-3">
                     <Image
                         src="/images/svg/logo.svg" 
@@ -19,7 +19,7 @@ export const Footer = () => {
                         width={128}
                         height={64}
                         className="h-22.5 lg:h-16 w-auto"/>
-                    {/* hidden for first official launch */}
+                    {/* hidden for first official launchr */}
                     <div className="hidden items-center flex gap-2">
                         <Image src="/images/github-logo.svg" alt="github" width={34} height={34}/>
                         <Link href="" target="_blank" className="text-black text-sm">available open source</Link>
@@ -33,18 +33,26 @@ export const Footer = () => {
                     ))}
                 </div>
                 <div className="gap-2.5 flex justify-center">
-                    <Link href="" target="_blank"><Facebook size={28} color="black"/></Link>
-                    <Link href="" target="_blank"><Linkedin size={28} color="black"/></Link>
-                    <Link href="" target="_blank"><Instagram size={28} color="black"/></Link>
-                    <Link href="" target="_blank"><Globe size={28} color="black"/></Link>
+                    <Link href="https://www.facebook.com/ChulalongkornUniversity" target="_blank" className="transition-transform duration-200 hover:scale-110">
+                        <Facebook size={28} color="black"/>
+                    </Link>
+                    <Link href="" target="_blank" className="transition-transform duration-200 hover:scale-110">
+                        <Linkedin size={28} color="black"/>
+                    </Link>
+                    <Link href="https://www.instagram.com/cuatclub.chula/" target="_blank" className="transition-transform duration-200 hover:scale-110">
+                        <Instagram size={28} color="black"/>
+                    </Link>
+                    <Link href="https://www.thinc.in.th/" target="_blank" className="transition-transform duration-200 hover:scale-110">
+                        <Globe size={28} color="black"/>
+                    </Link>
                 </div>
             </div>
             <div className="flex items-center flex-col justify-center lg:flex-row lg:justify-between">
                 <div className="text-[#8C8C8C] text-sm">Thinc. 2025 all right reserved</div>
-                <div className="text-[#8C8C8C] text-sm gap-8 flex underline">
+                {/* <div className="text-[#8C8C8C] text-sm gap-8 flex underline">
                     <Link href="" target="_blank">Privacy Policy</Link>
                     <Link href="" target="_blank">Privacy Reference</Link>
-                </div>
+                </div> */}
             </div>
         </footer>
     );
