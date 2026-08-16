4998f1f554a1601005b8e7afd33da0164ada44a5
Yanaphat Chatphokhinkun <yanaphat.chatphokhinkun@gmail.com>
2026-08-16 18:32:30 +0700

    fix: handle major risk [update undefined value]

--- src/server/api/modules/clubs/usecases/submit-club-profile-registration.usecase.ts
+++ src/server/api/modules/clubs/usecases/submit-club-profile-registration.usecase.ts
@@ -1,4 +1,7 @@
-import { clubsRepository } from "@/server/api/modules/clubs/clubs.repository";
+import {
+  clubsRepository,
+  type UpdateClubParams,
+} from "@/server/api/modules/clubs/clubs.repository";
 import type {
   SubmitClubProfileRegistrationInputDTO,
   SubmitClubProfileRegistrationOutputDTO,
@@ -10,25 +13,42 @@ export const submitProfileRegistration = async (
 ): Promise<SubmitClubProfileRegistrationOutputDTO> => {
   const { id, ...update } = input;
 
-  if (
-    !update.affiliationId &&
-    !update.shortDescription &&
-    !update.longDescription &&
-    !update.imageUrls &&
-    !update.contacts &&
-    !update.registrationStatus
-  ) {
+  if (Object.keys(update).length === 0) {
     throw validationError("Please provide at least one field to update.");
   }
 
-  await clubsRepository.updateById(id, {
-    affiliationId: update.affiliationId ?? undefined,
-    shortDescription: update.shortDescription ?? undefined,
-    longDescription: update.longDescription ?? undefined,
-    imageUrls: update.imageUrls ?? undefined,
-    contacts: update.contacts ?? undefined,
-    registrationStatus: update.registrationStatus ?? undefined,
-  });
+  let count = 0;
+  const updateData: Partial<UpdateClubParams> = {};
+  if ("affiliationId" in update && update.affiliationId !== undefined) {
+    updateData.affiliationId = update.affiliationId;
+    count++;
+  }
+  if ("shortDescription" in update && update.shortDescription !== undefined) {
+    updateData.shortDescription = update.shortDescription;
+    count++;
+  }
+  if ("longDescription" in update && update.longDescription !== undefined) {
+    updateData.longDescription = update.longDescription;
+    count++;
+  }
+  if ("imageUrls" in update && update.imageUrls !== undefined) {
+    updateData.imageUrls = update.imageUrls;
+    count++;
+  }
+  if ("contacts" in update && update.contacts !== undefined) {
+    updateData.contacts = update.contacts;
+    count++;
+  }
+  if ("registrationStatus" in update && update.registrationStatus !== undefined) {
+    updateData.registrationStatus = update.registrationStatus;
+    count++;
+  }
+
+  if (count === 0) {
+    throw validationError("Please provide at least one field to update.");
+  }
+
+  await clubsRepository.updateById(id, updateData);
 
   const club = await clubsRepository.getById(id);
   if (!club) throw notFound("Club not found");
