f516479d5da92f0334da21b29faccda0d28b9055
Yanaphat Chatphokhinkun <yanaphat.chatphokhinkun@gmail.com>
2026-08-23 15:13:50 +0700

    feat: add randomUUID to s3 key

--- src/server/api/modules/clubs/usecases/get-club-images-upload-url.usecase.ts
+++ src/server/api/modules/clubs/usecases/get-club-images-upload-url.usecase.ts
@@ -5,6 +5,7 @@ import {
 import { clubsRepository } from "@/server/api/modules/clubs/clubs.repository";
 import { getExtension, getPublicUrl, getSignedUploadUrl } from "@/server/services/r2";
 import { notFound } from "@/server/errors";
+import { randomUUID } from "crypto";
 
 export const getClubImagesUploadUrl = async (
   userId: string,
@@ -17,7 +18,7 @@ export const getClubImagesUploadUrl = async (
 
   const presignedUrls = await Promise.all(
     input.files.map(async (file, index) => {
-      const key = `clubs/${club.id}/image${index + 1}.${getExtension(file.contentType)}`;
+      const key = `clubs/${club.id}/${randomUUID()}/image${index + 1}.${getExtension(file.contentType)}`;
       const url = await getSignedUploadUrl(key, file.contentType);
       return { key, url, publicUrl: getPublicUrl(key) };
     })
