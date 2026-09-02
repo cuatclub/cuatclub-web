f9507e7c4d192a6df2395f09961b063d8b33c4e7
burapha-t <peeburapha@gmail.com>
2026-09-03 01:19:29 +0700

    fix: add application form url and fix not null field

--- src/server/db/schema/activities.ts
+++ src/server/db/schema/activities.ts
@@ -30,13 +30,14 @@ export const activities = pgTable(
       .notNull()
       .references(() => activityTypes.id, { onDelete: "restrict" }),
     title: text("title").notNull(),
-    description: text("description"),
-    posterUrl: text("poster_url"),
-    audiences: activityAudienceEnum("audiences").array().default([]).notNull(),
+    description: text("description").notNull(),
+    posterUrl: text("poster_url").notNull(),
+    audience: activityAudienceEnum("audience").notNull(),
     yearLevels: smallint("year_levels").array().default([]).notNull(),
     facultyIds: smallint("faculty_ids").array().default([]).notNull(),
-    applicationStartAt: timestamp("application_start_at", { withTimezone: true }),
-    applicationEndAt: timestamp("application_end_at", { withTimezone: true }),
+    applicationFormUrl: text("application_form_url").notNull(),
+    applicationStartAt: timestamp("application_start_at", { withTimezone: true }).notNull(),
+    applicationEndAt: timestamp("application_end_at", { withTimezone: true }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true })
       .defaultNow()
@@ -46,7 +47,7 @@ export const activities = pgTable(
   (t) => [
     index("activities_club_id_idx").on(t.clubId),
     index("activities_activity_type_id_idx").on(t.activityTypeId),
-    index("activities_audiences_idx").using("gin", t.audiences),
+    index("activities_audience_idx").on(t.audience),
     index("activities_year_levels_idx").using("gin", t.yearLevels),
     index("activities_faculty_ids_idx").using("gin", t.facultyIds),
     check(
